0d5b2116567d262736d799cc21abb6e3af4c1dff
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-05-31 12:16:40 +0100

    Added TodoList type and tested its equality

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,16 @@
 import Test.QuickCheck
 
+data Status = Complete | Active deriving (Show, Eq)
+type Item = (String, Status)
+type TodoList = [Item]
+
+todoListGen :: Gen TodoList
+todoListGen = elements [[(['A'..'Z'], Complete), (['A'..'Z'], Active)]]
+
+prop_TodoList_Equality :: Property
+prop_TodoList_Equality = forAll todoListGen (\todoList -> todoList == todoList)
+
+-- stack test
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+     quickCheck prop_TodoList_Equality
