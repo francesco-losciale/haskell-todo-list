23ba83e7366a1a93984d9033180de81655960c31
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-06-07 19:08:05 +0100

    mark element of list as complete, unit test setup

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,6 +8,9 @@ data TodoItem = Todo String Status deriving (Show, Eq)
 complete :: TodoItem -> TodoItem
 complete (Todo description status) = (Todo description Complete)
 
+apply :: (TodoItem -> TodoItem) -> TodoItem -> [TodoItem] -> [TodoItem]
+apply newStatus todo list = [ item | item <- list, item /= todo] ++ [(newStatus todo)]
+
 shouldCreateTodo = TestCase $
  assertEqual
     "should create a todo item"
@@ -17,21 +20,32 @@ shouldCreateTodo = TestCase $
 shouldAddTodoToList = TestCase $
  assertEqual
     "should add todo to list"
-        ((Todo "something" Active) : [])
         ([Todo "something" Active])
+        ((Todo "something" Active) : [])
 
 shouldMarkTodoAsCompleted = TestCase $
  assertEqual
     "given a todo then mark it as complete"
-        (complete (Todo "something" Active))
         (Todo "something" Complete)
+        (complete (Todo "something" Active))
+
+shouldMarkTodoAsCompletedInList = TestCase $
+ assertEqual
+    "given a todo list then mark a specific one as complete"
+        expectedList
+        (apply complete todo list)
+ where
+    todo = (Todo "something2" Active)
+    list = [(Todo "something1" Active), (Todo "something2" Active), (Todo "something3" Active)]
+    expectedList = [(Todo "something1" Active), (Todo "something2" Complete), (Todo "something3" Active)]
 
 
 -- hUnitTestToTests: Adapt an existing HUnit test into a list of test-framework tests
 tests = hUnitTestToTests $ TestList [
     TestLabel "shouldCreateTodo" shouldCreateTodo,
     TestLabel "shouldAddTodoToList" shouldAddTodoToList,
-    TestLabel "shouldMarkTodoAsCompleted" shouldMarkTodoAsCompleted
+    TestLabel "shouldMarkTodoAsCompleted" shouldMarkTodoAsCompleted,
+    TestLabel "shouldMarkTodoAsCompletedInList" shouldMarkTodoAsCompletedInList
  ]
 
 main = defaultMain tests
