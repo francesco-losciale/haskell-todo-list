770384f7e6d3bd2ebdc65c15ad96cd14ead722d5
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-06-20 02:06:54 +0100

    refactor unit test

--- test/unit/TodoSpec.hs
+++ test/unit/TodoSpec.hs
@@ -11,21 +11,21 @@ spec = do
     it "should validation of empty description fail" $ do
        (validate (Todo "" Active)) `shouldBe` (Left InvalidDescriptionError)
     it "should create todo to list" $ do
-       [item] `shouldBe` (createList item)
+       (createList todoActive) `shouldBe` [todoActive]
     it "should add todo to list" $ do
-       [item, item] `shouldBe` (collect (createList item) item)
+       (collect (createList todoActive) todoActive) `shouldBe` [todoActive, todoActive]
     it "should not add invalid todo to list" $ do
-       [] `shouldBe` (collect (createList invalidItem) invalidItem)
+       (collect (createList todoInvalid) todoInvalid) `shouldBe` []
     it "given a todo then mark it as complete" $ do
        (Todo "something" Complete) `shouldBe` (complete (Todo "something" Active))
     it "given a todo list then mark a specific one as complete" $ do
-       expectedList `shouldBe` (apply complete item list)
+       (apply complete todoActive todoList) `shouldBe` expectedUpdatedTodoList
     it "given a todo list then remove all the completed ones" $ do
-       expectedListWithoutComplete `shouldBe` (removeCompleted listWithComplete)
+       (removeCompleted todoList) `shouldBe` expectedPurgedTodoList
     where
-        item = (Todo "something" Active)
-        invalidItem = (Todo " " Active)
-        list = [(Todo "something1" Active), item, (Todo "something3" Active)]
-        expectedList = [(Todo "something1" Active), (Todo "something" Complete), (Todo "something3" Active)]
-        listWithComplete = [(Todo "something1" Active), (Todo "something2" Active), (Todo "something3" Complete)]
-        expectedListWithoutComplete = [(Todo "something1" Active), (Todo "something2" Active)]
+        todoInvalid = (Todo " " Active)
+        todoActive = (Todo "todo marked as active" Active)
+        todoComplete = (Todo "todo marked as complete" Complete)
+        todoList = [todoActive, todoComplete]
+        expectedUpdatedTodoList = [(Todo "todo marked as active" Complete), (Todo "todo marked as complete" Complete)]
+        expectedPurgedTodoList = [todoActive]
