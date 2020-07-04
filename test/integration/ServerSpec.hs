3883d3dfa9345a03d34b67a19b9db3891189eb5c
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-07-04 20:14:07 +0100

    compiled server unit test

--- test/integration/ServerSpec.hs
+++ test/integration/ServerSpec.hs
@@ -3,11 +3,28 @@ module ServerSpec where
 import Todo
 import Test.Hspec
 
+import Happstack.Lite
+-- http://happstack.com/page/view-page-slug/9/happstack-lite-tutorial
+import Network.Wreq
+import Control.Lens
+-- http://www.serpentine.com/wreq/tutorial.html
+
+
+import Data.ByteString.Lazy.UTF8 as BLU -- from utf8-string
+
+anApp :: ServerPart Happstack.Lite.Response
+anApp = msum
+  [
+    dir "hello" $ return (toResponse "hello")
+  ]
+
 spec :: Spec
 spec = beforeAll (setUp) $ do
-  describe "Spock Server" $ do
-    it "should web server start" $ do
-       True
+  describe "Web Server" $ do
+    it "should contact server" $ do
+        r <- get "http://localhost:8000/hello"
+        (r ^. responseBody) `shouldBe` (BLU.fromString "hello")
   where
     setUp = do
+              serve Nothing anApp
               return ()
