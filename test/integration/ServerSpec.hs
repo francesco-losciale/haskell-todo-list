0bd4d124489ee98b47cc5a0bcedb87c205f34eee
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-07-05 17:02:05 +0100

    got json decoding working

--- test/integration/ServerSpec.hs
+++ test/integration/ServerSpec.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
 module ServerSpec where
 
 import Todo
@@ -9,13 +12,25 @@ import Data.ByteString.Lazy.UTF8 as ByteString
 import Control.Lens
 import Control.Concurrent
 
+import Data.Aeson as A
+import Data.Data
+import Data.Maybe
+import Happstack.Server.Types
+import Control.Monad.IO.Class (liftIO)
 
 anApp :: ServerPart Server.Response
 anApp = msum
   [
-    dir "hello" $ return (toResponse "hello"),
-    dir "get-empty-json" $ return (toResponse "{}")
+    Server.dir "hello" $ return (toResponse "hello"),
+    Server.dir "get-empty-json" $ return (toResponse "{}"),
+    Server.dir "get-unit" $ return (toResponse "{\"x\":1,\"y\":2}")
   ]
+
+data Unit = Unit { x :: Int, y :: Int } deriving (Show, Eq, Data, Typeable)
+instance FromJSON Unit where
+    parseJSON = withObject "Unit" $ \v -> Unit
+        <$> v .: "x"
+        <*> v .: "y"
 
 spec :: Spec
 spec = beforeAll (setUp) $ do
@@ -26,6 +41,10 @@ spec = beforeAll (setUp) $ do
     it "should get empty json from server" $ do
         response <- Client.get "http://localhost:8000/get-empty-json"
         ByteString.toString(response ^. responseBody) `shouldBe` "{}"
+    it "should decode json " $ do
+        response <- Client.get "http://localhost:8000/get-unit"
+        let unit = fromJust $ A.decode (response ^. responseBody) :: Unit
+        unit `shouldBe` (Unit {x=1,y=2})
   where
     setUp = do
               forkIO (serve Nothing anApp)
