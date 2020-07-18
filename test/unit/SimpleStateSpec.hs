9906509d067e82c69676de6d4bb04f1d7be1ac76
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-07-18 10:58:27 +0100

    refined SimpleStateSpec

--- test/unit/SimpleStateSpec.hs
+++ test/unit/SimpleStateSpec.hs
@@ -13,13 +13,29 @@ bindSt simpleState calculateResultAndInjectInSimpleState =
         \state -> let (result, newState) = simpleState state
                   in (calculateResultAndInjectInSimpleState result) newState
 
+getSt :: SimpleState state state
+getSt = \state -> (state, state)
+
+putSt :: state -> SimpleState state ()
+putSt state = \_ -> ((), state)
+
 spec :: Spec
 spec = do
   describe "Simple example of a state transformer" $ do
       it "Given separate value and state, apply state transformer" $ do
         ((returnSt value) state) == (value, state)
-      it "Given a state, transform the value and not the state" $ do
+      it "Given a state, transform the value but not the state" $ do
         ((returnSt value) `bindSt` calculateResultAndInjectInSimpleState) state == ("result", state)
+      it "Given a state, get a SimpleState out of it" $ do
+        getSt state == (state, state)
+      it "Given a state, ignore it and replace with a new one" $ do
+        putSt 2 state == ((), 2)
+      it "Given a state, get a SimpleState out of it and bind with putSt" $ do
+        (getSt `bindSt` putSt) state == ((), 1)
+      it "Given a state, get a SimpleState out of it and bind with function that changes state" $ do
+        (getSt `bindSt` (\_ -> putSt 2)) state == ((), 2)
+      it "Given a state, get a SimpleState, change the state, calculate a result" $ do
+        (getSt `bindSt` (\_ -> putSt 2) `bindSt` calculateResultAndInjectInSimpleState) state == ("result", 2)
   where
     value = "value"
     state = 1
