6ef7b4b08ad00408fbcfe7015d7e38b7b6bb6034
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-08-05 16:25:57 +0200

    test works now, using a pair of integer for new state and old state

--- test/unit/SimpleStateTransactionalSpec.hs
+++ test/unit/SimpleStateTransactionalSpec.hs
@@ -5,29 +5,30 @@ import StateMonadSpec
 
 data TransactionalStep = Begin | Commit deriving Eq
 
-begin :: State Integer TransactionalStep
+begin :: State (Integer, Integer) TransactionalStep
 begin = do 
-          getSt 
+          (s,s') <- getSt
+          putSt (s',s') 
           return(Begin)
 
-commit :: State Integer TransactionalStep
+commit :: State (Integer, Integer) TransactionalStep
 commit = do 
           getSt 
           return(Commit)          
 
-atomically :: (Integer -> Integer) -> State Integer TransactionalStep
+atomically :: (Integer -> Integer) -> State (Integer, Integer) TransactionalStep
 atomically f = do 
-            s <- getSt
+            (s, s') <- getSt
             case s of
-              1 -> putSt (f s) >> begin
-              2 -> putSt (f s) >> commit
+              1 -> putSt ((f s), s') >> begin
+              2 -> putSt ((f s), s') >> commit
 
 
 spec = do
   describe "SimpleStateTransactional: basic functions" $ do
       it "Can open transactions" $ do
-        runState (atomically (+1)) 1 == (Begin,1)       
+        runState (atomically (+1)) (1,1) == (Begin,(1,1))       
       it "Can commit transactions" $ do
-        runState (atomically (+1)) 2 == (Commit,3)           
+        runState (atomically (+1)) (2,2) == (Commit,(3,2))           
    
    
