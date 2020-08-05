505a42ee8967a99072d2013af087d2926f28e511
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-08-05 16:34:34 +0200

    test for rollback added

--- test/unit/SimpleStateTransactionalSpec.hs
+++ test/unit/SimpleStateTransactionalSpec.hs
@@ -3,7 +3,7 @@ module SimpleStateTransactionalSpec where
 import Test.Hspec
 import StateMonadSpec
 
-data TransactionalStep = Begin | Commit deriving Eq
+data TransactionalStep = Begin | Commit | Rollback deriving Eq
 
 begin :: State (Integer, Integer) TransactionalStep
 begin = do 
@@ -16,12 +16,19 @@ commit = do
           getSt 
           return(Commit)          
 
+rollback :: State (Integer, Integer) TransactionalStep
+rollback = do
+          (s,s') <- getSt      
+          putSt (s',s')
+          return(Rollback) 
+
 atomically :: (Integer -> Integer) -> State (Integer, Integer) TransactionalStep
 atomically f = do 
             (s, s') <- getSt
             case s of
               1 -> putSt ((f s), s') >> begin
               2 -> putSt ((f s), s') >> commit
+              3 -> putSt ((f s), s') >> rollback
 
 
 spec = do
@@ -29,6 +36,8 @@ spec = do
       it "Can open transactions" $ do
         runState (atomically (+1)) (1,1) == (Begin,(1,1))       
       it "Can commit transactions" $ do
-        runState (atomically (+1)) (2,2) == (Commit,(3,2))           
+        runState (atomically (+1)) (2,2) == (Commit,(3,2))  
+      it "Can rollback transactions" $ do
+        runState (atomically (+1)) (3,3) == (Rollback,(3,3))           
    
    
